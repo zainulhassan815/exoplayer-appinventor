e5354beb350ca17813d45c5babe8b1af578bb259
zainulhassan815 <zainulhassan4330@gmail.com>
2021-10-19 22:02:23 +0500

    Fixed `console.e is not a function`

--- docs/script.js
+++ docs/script.js
@@ -11,7 +11,7 @@ function copyTextToClipboard(text) {
   navigator.clipboard.writeText(text).then(function() {
     console.log('Async: Copying to clipboard was successful!');
   }, function(err) {
-    console.error('Async: Could not copy text: ', err);
+    console.log('Async: Could not copy text: ', err);
   });
 }
 
