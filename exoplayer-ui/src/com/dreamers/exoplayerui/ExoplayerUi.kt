08615887013b398633faf8828e8c600226c7bc58
Zain Ul Hassan <zainulhassan4330@gmail.com>
2021-10-27 00:37:09 +0500

    Additional `flags` for `fullscreen`

--- exoplayer-ui/src/com/dreamers/exoplayerui/ExoplayerUi.kt
+++ exoplayer-ui/src/com/dreamers/exoplayerui/ExoplayerUi.kt
@@ -283,6 +283,8 @@ class ExoplayerUi(container: ComponentContainer) : AndroidNonvisibleComponent(co
         window.decorView.systemUiVisibility = (
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                         // Hide the nav bar and status bar
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         or View.SYSTEM_UI_FLAG_FULLSCREEN)
 
